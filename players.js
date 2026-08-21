f3ce4e132ee867020c24762510e32e19e7759f66
Angus Franklin <angus.franklin.me@gmail.com>
2026-08-21 10:10:18 +0000

    players.js file started and pushed all js files to gun_master.html

---
+++ players.js
@@ -0,0 +1,11 @@
+class players{
+    constructor(health, damage, speed, armor){
+        this.health = health;
+        this.damage = damage;
+        this.speed = speed;
+        this.armor = armor;
+    }
+    move(){
+
+    }
+}
