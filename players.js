61db39c5a791491a2e5c7893496c096687503045
Angus Franklin <angus.franklin.me@gmail.com>
2026-08-22 01:40:03 +0000

    finished constructor and created functions for moving left and right

--- players.js
+++ players.js
@@ -1,11 +1,19 @@
 class players{
-    constructor(health, damage, speed, armor){
+    constructor(health, damage, speed, attack_speed, projectile_speed, element){ //initiating stats depending on their class
         this.health = health;
         this.damage = damage;
         this.speed = speed;
-        this.armor = armor;
+        this.attack_speed = attack_speed;
+        this.projectile_speed = projectile_speed;
+        this.element = element; //the actual html tag of the player's image
+        this.hitbox = this.element.getBoundingClientRect(); //player's hitbox
     }
-    move(){
+    
+    run_forward(speed){
+        //tilemap move left
+    }
 
+    run_backward(speed){
+        //tilemapmove backward
     }
 }
